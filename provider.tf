a60ce5299767fc0bf9c09ef0b09f8b7a70258558
Devopsproject1234 <152521021+Devopsproject1234@users.noreply.github.com>
2023-12-09 11:17:02 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,5 @@
 provider "azurerm" {
   features {}
-}
-provider "azurerm" {
  subscription_id = "4d5d3460-ece5-4648-bb78-f1a47e2f8547"
  client_id       = "d85a659d-7e8f-4599-98b3-b4778d2c1805"
  client_secret   = "sfo8Q~k~1.~StgXOTPNmxUAJnKz8gWvcE~XBtao2"
